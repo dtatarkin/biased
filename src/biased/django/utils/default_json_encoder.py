a49d7b66942713cea32c54f4b1f785713101d633
Dmitry Tatarkin <tatarkin@gmail.com>
2025-09-30 14:41:07 +0300

    Remove Django as a dependency for the `DefaultJsonEncoder`

--- src/biased/django/utils/default_json_encoder.py
+++ src/biased/django/utils/default_json_encoder.py
@@ -1,23 +1,65 @@
-from datetime import datetime
+from datetime import date, datetime, time, timedelta
 from decimal import Decimal
 from enum import Enum
+from json import JSONEncoder
+from uuid import UUID
 
-from django.core.serializers.json import DjangoJSONEncoder
-from plaid.model_utils import ModelNormal
 from pydantic import AnyUrl, BaseModel
 from structlog.processors import _json_fallback_handler
 from ulid import ULID
 
 
-class DefaultJsonEncoder(DjangoJSONEncoder):
+def _is_aware(value: time):
+    return value.utcoffset() is not None
+
+
+def _get_duration_components(duration: timedelta):
+    days = duration.days
+    seconds = duration.seconds
+    microseconds = duration.microseconds
+
+    minutes = seconds // 60
+    seconds %= 60
+
+    hours = minutes // 60
+    minutes %= 60
+
+    return days, hours, minutes, seconds, microseconds
+
+
+def _duration_iso_string(duration: timedelta):
+    if duration < timedelta(0):
+        sign = "-"
+        duration *= -1
+    else:
+        sign = ""
+
+    days, hours, minutes, seconds, microseconds = _get_duration_components(duration)
+    ms = f".{microseconds:06d}" if microseconds else ""
+    return f"{sign}P{days}DT{hours:02d}H{minutes:02d}M{seconds:02d}{ms}S"
+
+
+class DefaultJsonEncoder(JSONEncoder):
     def default(self, o):
         if isinstance(o, datetime):
-            return o.isoformat().replace("+00:00", "Z")
-        if isinstance(o, Decimal):
-            return str(o)
-        if isinstance(o, ULID):
-            return o.str
-        if isinstance(o, AnyUrl):
+            r = o.isoformat()
+            if o.microsecond:
+                r = r[:23] + r[26:]
+            if r.endswith("+00:00"):
+                r = r.removesuffix("+00:00") + "Z"
+            return r
+        if isinstance(o, date):
+            return o.isoformat()
+        if isinstance(o, time):
+            if _is_aware(o):
+                raise ValueError("JSON can't represent timezone-aware times.")
+            r = o.isoformat()
+            if o.microsecond:
+                r = r[:12]
+            return r
+        if isinstance(o, timedelta):
+            return _duration_iso_string(o)
+        if isinstance(o, (Decimal, UUID, ULID, AnyUrl)):
             return str(o)
         if isinstance(o, BaseModel):
             return o.model_dump()
@@ -25,8 +67,6 @@ class DefaultJsonEncoder(DjangoJSONEncoder):
             return o.value
         if isinstance(o, bytes):
             return o.decode()
-        if isinstance(o, ModelNormal):
-            return o.to_dict()
         return super().default(o)
 
 
