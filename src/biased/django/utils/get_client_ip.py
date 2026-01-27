5e802532953139acb70805f306dc87021d8a9730
Dmitry Tatarkin <tatarkin@gmail.com>
2026-01-27 15:58:03 +0300

    get_client_ip typehints

--- src/biased/django/utils/get_client_ip.py
+++ src/biased/django/utils/get_client_ip.py
@@ -1,7 +1,7 @@
 from django.http import HttpRequest
 
 
-def get_client_ip(request: HttpRequest) -> str:
+def get_client_ip(request: HttpRequest) -> str | None:
     x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
     if x_forwarded_for:
         ip = x_forwarded_for.split(",")[0]
